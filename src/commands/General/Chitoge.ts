c4d8cc0f577bce4a0c6d2719be86d6beafe8c8cb
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-06-07 16:22:06 +0530

    Create Chitoge.ts

---
+++ src/commands/General/Chitoge.ts
@@ -0,0 +1,34 @@
+/** @format */
+
+import { MessageType, Mimetype } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
+export default class Command extends BaseCommand {
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "chitoge",
+			description: "Displays the info",
+			category: "general",
+			usage: `${client.config.prefix}yotsuba`,
+			baseXp: 2000,
+		});
+	}
+
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const chitoge =
+			"https://c.tenor.com/sr7BjZ_-StIAAAPo/marin-kitagawa.mp4";
+		return void this.client.sendMessage(
+			M.from,
+			{ url: chitoge },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
+				caption: `⚡ *✴🎀𝓜𝓐𝓡𝓘𝓝🎀✴* ⚡\n\n🍀 *Description: repo is private better luck next time.*\n\n🌐 *insta id: https://www.instagram.com/akuma__24/?hl=en* \n\n 📒\n`,
+			}
+		);
+	};
+}
