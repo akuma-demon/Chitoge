84cc2f6a3a3f5fbb7c080e6d15232621853458df
akumazero <102664326+akumasugi@users.noreply.github.com>
2022-06-07 16:14:41 +0530

    Create Do-you-mean.ts

---
+++ src/commands/General/Do-you-mean.ts
@@ -0,0 +1,45 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { join } from 'path'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: '',
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        const n = [
+            'https://c.tenor.com/sr7BjZ_-StIAAAPo/marin-kitagawa.mp4',
+            'https://c.tenor.com/53rodm0H7VoAAAPo/kitagawa-kitagawa-marin.mp4',
+        ]
+        let chitoge = n[Math.floor(Math.random() * n.length)]
+        return void this.client.sendMessage(M.from, { url: chitoge }, MessageType.video, {
+            mimetype: Mimetype.gif,
+            caption: `Do you mean *${this.client.config.prefix}help*? \n` }
+        )
+    }
+}
+          
+       
+
+
+    
+        
+           
+           
+            
+            
+        
+    
+
+    
+        
+           
+           
+           
+   
